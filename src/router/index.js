40be681bdfe947c5f58d1d8e5595060074999a6e
大薄荷 <2372108972@qq.com>
2022-06-16 21:12:08 +0800

    完成了报表功能的开发

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,7 @@ import GoodsList from '../components/goods/List.vue'
 import Add from '../components/goods/Add.vue'
 import Edit from '../components/goods/Edit.vue'
 import Order from '../components/order/Order.vue'
+import Report from '../components/report/Report.vue'
 
 Vue.use(VueRouter)
 
@@ -27,9 +28,10 @@ const routes = [
       { path: '/categories', component: Cate },
       { path: '/params', component: Params },
       { path: '/goods', component: GoodsList },
-      { path: '/goods/add', component: Add},
-      { path: '/goods/edit', component: Edit},
-      { path: '/orders', component: Order}
+      { path: '/goods/add', component: Add },
+      { path: '/goods/edit', component: Edit },
+      { path: '/orders', component: Order },
+      { path: '/reports', component: Report }
     ]
   },
 ]
